8dc53c7636b9a3f4730bf1a153f6663376cbfcba
TendoCryzx <amfinezxc@hotmail.com>
2024-01-08 09:53:46 +0700

    d

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -8,16 +8,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-  taskId!: number; 
+  idtask!: number; 
   taskDetails: any;
 
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.taskId = +params['id']; 
+      this.idtask = +params['id']; 
   
-      this.http.get(`http://103.13.31.37:17444/api/tasks/${this.taskId}`)
+      this.http.get(`http://103.13.31.37:17444/api/tasks/${this.idtask}`)
         .subscribe((data: any) => {
           this.taskDetails = data;
   
